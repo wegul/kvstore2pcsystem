a0efe55dd13fd224e76fafb860bdec98dfc6a74f
swg <suweigao1@gmail.com>
2020-05-24 14:03:45 +0800

    未实现方法

---
+++ src/test/java/com/lab3/kvstore2pcsystem/Kvstore2pcsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab3.kvstore2pcsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kvstore2pcsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
